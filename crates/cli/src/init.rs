cff9fd84e3a75bafd6ef5abe66d49f95a24c884a
amr-crabnebula <amr@crabnebula.dev>
2024-05-09 23:53:29 +0300

    Add `define_fuzz_target` macro

--- crates/cli/src/init.rs
+++ crates/cli/src/init.rs
@@ -1,5 +1,6 @@
 use std::{
     cmp::Ordering,
+    collections::BTreeMap,
     ffi::OsStr,
     fs,
     io::Write,
@@ -8,8 +9,10 @@ use std::{
 
 use anyhow::Context;
 use clap::Parser;
+use handlebars::Handlebars;
 use ignore::WalkBuilder;
 use include_dir::{include_dir, Dir};
+use serde::Serialize;
 
 const TAURI_JSON: &str = "tauri.conf.json";
 const TAURI_JSON5: &str = "tauri.conf.json5";
@@ -83,7 +86,13 @@ pub fn tauri_dir() -> anyhow::Result<PathBuf> {
 
 const TEMPLATE_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/template");
 
-fn render_with_generator<P: AsRef<Path>, F: FnMut(PathBuf) -> std::io::Result<Option<fs::File>>>(
+fn render_with_generator<
+    P: AsRef<Path>,
+    S: Serialize,
+    F: FnMut(PathBuf) -> std::io::Result<Option<fs::File>>,
+>(
+    handlebars: &mut Handlebars,
+    data: &S,
     dir: &Dir<'_>,
     out_dir: P,
     out_file_generator: &mut F,
@@ -100,21 +109,31 @@ fn render_with_generator<P: AsRef<Path>, F: FnMut(PathBuf) -> std::io::Result<Op
         }
 
         if let Some(mut output_file) = out_file_generator(file_path)? {
-            output_file.write_all(file.contents())?
+            if let Some(contents) = file.contents_utf8() {
+                let rendered = handlebars.render_template(contents, data)?;
+                output_file.write_all(rendered.as_bytes())?
+            } else {
+                output_file.write_all(file.contents())?
+            }
         }
     }
 
     for dir in dir.dirs() {
-        render_with_generator(dir, out_dir, out_file_generator)?;
+        render_with_generator(handlebars, data, dir, out_dir, out_file_generator)?;
     }
 
     Ok(())
 }
 
-fn render<P: AsRef<Path>>(dir: &Dir<'_>, out_dir: P) -> anyhow::Result<()> {
+fn render<P: AsRef<Path>, S: Serialize>(
+    handlebars: &mut Handlebars,
+    data: &S,
+    dir: &Dir<'_>,
+    out_dir: P,
+) -> anyhow::Result<()> {
     let out_dir = out_dir.as_ref();
     let mut created_dirs = Vec::new();
-    render_with_generator(dir, out_dir, &mut |file_path: PathBuf| {
+    render_with_generator(handlebars, data, dir, out_dir, &mut |file_path: PathBuf| {
         let path = out_dir.join(file_path);
         let parent = path.parent().unwrap().to_path_buf();
         if !created_dirs.contains(&parent) {
@@ -130,16 +149,47 @@ pub struct Options {
     /// Set target directory for init
     #[clap(short, long)]
     directory: Option<PathBuf>,
+    /// Set the crate name to be fuzzed
+    #[clap(short, long)]
+    crate_name: Option<String>,
 }
 
 pub fn command(options: Options) -> anyhow::Result<()> {
-    let dir = match options.directory {
+    let cwd = match options.directory {
         Some(dir) => dir,
         None => tauri_dir()?,
     };
 
-    let dir = dir.join("fuzz");
+    let fuzz_dir = cwd.join("fuzz");
+    fs::create_dir_all(&fuzz_dir)?;
 
-    fs::create_dir_all(&dir)?;
-    render(&TEMPLATE_DIR, dir)
+    let mut handlebars = Handlebars::new();
+    handlebars.register_escape_fn(handlebars::no_escape);
+
+    let mut data = BTreeMap::new();
+
+    let crate_name = match options.crate_name {
+        Some(name) => name,
+        None => {
+            let cargo_toml = std::fs::read_to_string(cwd.join("Cargo.toml"))
+                .with_context(|| format!("Couldn't find `Cargo.toml` in: {}", cwd.display()))?;
+
+            let cargo_toml: toml::Value = toml::from_str(&cargo_toml)?;
+            cargo_toml
+                .get("package")
+                .and_then(|p| p.get("name"))
+                .and_then(|n| n.as_str())
+                .ok_or_else(|| {
+                    anyhow::anyhow!(
+                        "Cargo.toml doesn't have a `name` field, try using `--crate-name` flag"
+                    )
+                })?
+                .to_string()
+        }
+    };
+
+    data.insert("crate_name_underscored", crate_name.replace('-', "_"));
+    data.insert("crate_name", crate_name);
+
+    render(&mut handlebars, &data, &TEMPLATE_DIR, fuzz_dir)
 }
--- crates/fuzzer/src/lib.rs
+++ crates/fuzzer/src/lib.rs
@@ -5,3 +5,54 @@ pub use crate::fuzzer_options::SimpleFuzzerConfig;
 
 #[cfg(feature = "tauri")]
 pub mod tauri_utils;
+
+#[macro_export]
+macro_rules! define_fuzz_target {
+    (
+        command: $command:literal,
+        path: $path:path,
+        parameters: {
+            $($param:ident : $param_fn:expr),+ $(,)?
+        },
+        policy: $policy:expr $(,)?
+    ) => {
+        use ::fuzzer::tauri_utils::{create_invoke_payload, invoke_command_minimal, CommandArgs};
+        use ::libafl::inputs::{BytesInput, HasBytesVec};
+        use ::libafl::prelude::ExitKind;
+        use ::tauri::test::{mock_builder, mock_context, noop_assets, MockRuntime};
+        use ::tauri::App as TauriApp;
+        use ::tauri::InvokePayload;
+
+        const COMMAND_NAME: &str = $command;
+
+        fn main() {
+            let ptr = harness as *const ();
+            let fuzz_dir = std::path::PathBuf::from(std::env!("CARGO_MANIFEST_DIR"));
+            let fuzz_config_file = fuzz_dir.join("fuzzer_config.toml");
+            let options = fuzzer::SimpleFuzzerConfig::from_toml(fuzz_config_file, COMMAND_NAME, fuzz_dir).into();
+            ::fuzzer::fuzz_main(harness, options, ptr as usize, $policy);
+        }
+
+        fn harness(input: &BytesInput) -> ExitKind {
+            let app = mock_builder()
+                .invoke_handler(tauri::generate_handler![$path])
+                .build(mock_context(noop_assets()))
+                .expect("Failed to init Tauri app");
+
+            let _ = invoke_command_minimal(app, create_payload(input.bytes()));
+
+            ExitKind::Ok
+        }
+
+        fn create_payload(bytes: &[u8]) -> InvokePayload {
+            let mut params = CommandArgs::new();
+
+            $(
+                let param = $param_fn(bytes);
+                params.insert(stringify!($param).to_string(), param);
+            )*
+
+            create_invoke_payload(None, COMMAND_NAME, params)
+        }
+    }
+}
